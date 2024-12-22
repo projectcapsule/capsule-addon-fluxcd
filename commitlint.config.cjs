e25078da723a7e5635fa869fc5bd8d1c5b8d0827
Massimiliano Giovagnoli <me@maxgio.it>
2024-12-22 10:49:55 +0100

    chore(.github): bump commitlint and add config
    Signed-off-by: Massimiliano Giovagnoli <me@maxgio.it>

---
+++ commitlint.config.cjs
@@ -0,0 +1,20 @@
+const Configuration = {
+    extends: ['@commitlint/config-conventional'],
+    plugins: ['commitlint-plugin-function-rules'],
+    rules: {
+      'type-enum': [2, 'always', ['chore', 'ci', 'docs', 'feat', 'test', 'fix', 'sec']],
+      'body-max-line-length': [1, 'always', 500],
+      'subject-case': [2, 'always', ['lower-case', 'sentence-case', 'upper-case']],
+    },
+    /*
+     * Whether commitlint uses the default ignore rules, see the description above.
+     */
+    defaultIgnores: true,
+    /*
+     * Custom URL to show upon failure
+     */
+    helpUrl:
+      'https://github.com/projectcapsule/capsule-addon-fluxcd?tab=readme-ov-file#development',
+  };
+  
+  module.exports = Configuration;
